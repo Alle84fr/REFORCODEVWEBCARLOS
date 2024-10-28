3e9cae42763a63327cc571f18bde45b87f9c15f9
Alesssandra F R <arfurlanetto@gmail.com>
2024-10-28 01:39:58 -0300

    adicionando novos métodos

--- cursomslpoo/exe1.py
+++ cursomslpoo/exe1.py
@@ -5,13 +5,90 @@ class Jogo:
       self.fimDeJogo = False
       self.participante1 = Participante()
       self.participantes2 = Participante()
+      
+   def start (self):
+      rodada = Rodada(self.participante1, self.participantes2)
    
+   def condicaoDefimDeJogo(self):
+      print(" implement, adicone lógica futura")
+      
+   def ganhador(self):
+      pprint(" implement, adicone lógica futura")
+
    
 class Participante:
-   def __init__(self):
+   def __init__(self, name):
       self.pontos = 0
       self.escolha = ""
+      self.name = name
+      
+   def escolha(self):
+      input(f"{self.name}, selecione pedra, papel ou tesoura: ")
+      print(f"{self.name} ecolheu {self.escolha}")
+      
+   def numerosDeChoise(self):
+      switcherTroca = {
+         "pedra": 0,
+         "papel": 1,
+         "tesoura": 2
+      }
+      return switcherTroca [self.escolha]
 
-class Rodada:
+      
+#adicionando adiconado name como parâmentro e função chance
+#input = > {name} é um espaço reservado (placeholder) que será subtituido polo valor da variável name - "{name}, selecione pedra, papel ou tesoura:" esta parte significa que é uma string formatada
+#final do input = > o método .format() que mostra a "referência de onde irá tirar a informação que será substituida"
+#ex: self.name = Abidu - ao mostrar na tela aparecerá - Abidu, selecione pedra, papel ou tesoura: 
+
+#duas formas de escrever, usarei f-string 
+# input("{name}, selecione pedra, papel ou tesoura: ".format(name=self.name))
+# input(f"{self.name}, selecione pedra, papel ou tesoura: ")
+# print("{name} ecolheu {choise}".format(name = self.name, choise = self.chance))
+# print(f"{self.name} ecolheu {self.chance}")
+
+
+
+ class Rodada:
    
+   def __init__(self, p1, p2):
+      self.rules = [
+         [0, -1, 1],
+         [1, 0, -1],
+         [-1, 1, 0]
+      ]
+      
+      p1 escolha()
+      p2 escolha()
+      
+      result = self.compareEscolha(p1, p2)
+      print(f"Resultado da partida {self.getResult(result)}")
+   
+   def comparacao(self):
+      print(" implement, adicone lógica futura")
+   
+   def awardPointsPremiarPontos(self):
+      print(" implement, adicone lógica futura")
+      
+   def compareEscolha(self, p1, p2):
+      return self.rule[p1.numerosDeChoise()][p2.numerosDeChoise()]
 
+#def compareEscolha permite a comparação e escolher o vencedor
+
+   def getResult(self, result):
+      res = {
+         0: "empate",
+         1: "vendedor"
+         -1: "perdedor"
+      }
+      return res [result]
+
+#esse método ajudará a determinar o resultado e imprimirá um texto fácil de entender na tela.
+# O código anterior introduz o campo rules, que contém uma implementação das regras para pedra, papel e tesoura. Além disso, a #chamada a self.compareChoices() compara duas escolhas feitas. Por fim, há uma linha que imprime resultados amigáveis para leitores #na tela.
+
+#principal
+
+game = Jogo()
+
+game.start()
+
+# estou chamando a classe Jogo para que seja criada, e depois estou chamndo o MÉTODO, MÉTODO start que está dentro de Jogo e estou chamando este método start na variável game
--- cursomslpoo/exer.md
+++ cursomslpoo/exer.md
@@ -4,3 +4,63 @@
 - título, autor, quantidade de pag, editora, tipo, edição, cdd
 
 - o que farei, variáveis para cada um deles, que o objeto receberá e depois devolverá
+
+2. A primeira parte do jogo envolve configurá-lo, o que significa criar uma instância do próprio jogo e levar o jogo para um ponto em que ele fique aguardando a ação dos participantes.
+
+class Participant:
+
+    def __init__(self):
+
+        self.points = 0
+
+        self.choice = ""
+
+class GameRound:
+
+class Game:
+
+    def __init__(self):
+
+        self.endGame = False
+
+        self.participant = Participant()
+
+        self.secondParticipant = Participant()
+
+-  para criar um programa em estilo OOP (object oriented programming)/ poo (programação orientada a objetos), primeiro você modela e depois codifica. A modelagem produziu a saída de uma tabela que representava por quais objetos, dados e comportamento o seu programa parece ser composto.
+
+Desta vez vamos preencher com métodos que serão adicionados à classe e adicionar código a esse método paraq ue funcioname da maneira que deveriam
+
+class Participant:
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
+        self.choice = ""
+    def choose(self):
+        self.choice = input("{name}, select rock, paper or scissor: ".format(name= self.name))
+        print("{name} selects {choice}".format(name=self.name, choice = self.choice))
+
+class GameRound:
+    def __init__(self, p1, p2):
+        p1.choose()
+        p2.choose()
+    def compareChoices(self):
+        print("implement")
+    def awardPoints(self):
+        print("implement")
+
+class Game:
+    def __init__(self):
+        self.endGame = False
+        self.participant = Participant("Spock")
+        self.secondParticipant = Participant("Kirk")
+    def start(self):
+        game_round = GameRound(self.participant, self.secondParticipant)
+
+    def checkEndCondition(self):
+        print("implement")
+    def determineWinner(self):
+        print("implement")
+
+game = Game()
+game.start()
